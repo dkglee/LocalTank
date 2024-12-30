c808184be019bf4d60061b4284b6602f542d8ad3
dkglee <ideade9@gmail.com>
2024-12-30 20:42:17 +0900

    Test Merge Conflict

--- Source/MyProject/MyProject.Build.cs
+++ Source/MyProject/MyProject.Build.cs
@@ -14,7 +14,8 @@ public class MyProject : ModuleRules
 
 		PublicIncludePaths.AddRange(new string[]
 		{
-			"MyProject/deulee/Public"	
+			"MyProject/deulee/Public",
+			"MyProject/Private",
 		});
 
 		// Uncomment if you are using Slate UI
